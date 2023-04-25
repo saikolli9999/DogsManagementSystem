e091340e13432d4d0de8ab9fbba22575bd657156
Sai Kumar Kolli <s548405@nwmissouri.edu>
2023-04-25 03:02:25 -0500

    Committing and Deploying DMS project

---
+++ src/main/java/com/fellahClass/DMS/Repository/DogRepository.java
@@ -0,0 +1,11 @@
+package com.fellahClass.DMS.Repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fellahClass.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog,Integer> {
+List<Dog> findByName(String name);
+}
---
+++ src/main/java/com/fellahClass/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.fellahClass.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.fellahClass.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer,Integer> {
+
+}
