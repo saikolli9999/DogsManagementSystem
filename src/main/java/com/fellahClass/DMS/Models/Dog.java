f58ac46540a86ce4c27399e32e17c50ec6250da2
Sai Kumar Kolli <s548405@nwmissouri.edu>
2023-04-29 16:19:53 -0500

    changed

--- src/main/java/com/fellahClass/DMS/Models/Dog.java
+++ src/main/java/com/fellahClass/DMS/Models/Dog.java
@@ -1,8 +1,8 @@
 package com.fellahClass.DMS.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Dog {
